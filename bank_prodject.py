091759d6a7517a7efd59e83e5b046b57b5fbb795
Luca Bazzea <luca.bazzea@gmail.com>
2020-12-01 13:03:19 +0200

    Fixed error on mainMenu function call

--- bank_prodject.py
+++ bank_prodject.py
@@ -15,46 +15,51 @@ class Account:
     def Balance(self):
         return self.balance
 
-    def setValue(self, id, balance):
+    @Balance.setter
+    def Balance(self, balance):
+        self.balance = balance
 
-        id = randint(1000, 9999)
-        balance = randint(0.0, 10000000.0)
+    def accStatus(self, ID, Balance):
 
-    def accStatus(self, id, balance):
+        print(f"Acc No. {self.ID} Balance: R{self.Balance}")
 
-        return f"Acc No. {self.id} Balance: R{self.balance}"
-
-    def withdraw(self, id, balance):
+    def withdraw(self, ID, Balance):
 
         withdrawn = int(input("How much would you like to withdraw?\n"))
 
-        balance -= withdrawn
+        if Balance < withdrawn:
+            print("Insufficient Funds")
 
-    def deposit(self, id, balance):
+        elif Balance >= withdrawn:
+            Balance -= withdrawn
+
+    def deposit(self, ID, Balance):
 
         deposited = int(input("How much would you like to deposit?\n"))
 
-        balance -= deposited
+        Balance += deposited
 
-    class Savings:
+class Savings:
 
-        def __init__(self, yearlyIntrestRate:float):
+    def __init__(self, yearlyIntrestRate:float):
 
-            self.yearlyIntrestRate = yearlyIntrestRate
+        self.yearlyIntrestRate = yearlyIntrestRate
 
-        def getMonthlyIntrest(self, monthlyIntrest:float, yearlyIntrestRate, Balance):
+    def getMonthlyIntrest(self, monthlyIntrest:float, yearlyIntrestRate, Balance):
 
-            monthlyIntrestRate = yearlyIntrestRate / 12
+        monthlyIntrestRate = yearlyIntrestRate / 12
 
-            monthlyIntrest = Balance * monthlyIntrestRate
+        monthlyIntrest = Balance * monthlyIntrestRate
 
-            return monthlyIntrest
+        return monthlyIntrest
 
-class ATM:
+class ATM(Account):
+
+    def __init__(self, id, balance):
+        super().__init__(id, balance)
 
     def mainMenu(self, id, balance):
 
-        account = Account(id, balance)
 
         userID = [1234, 2345]
 
@@ -62,30 +67,31 @@ class ATM:
 
             while True:
 
-                inputID = int(input("Please enter your Acc. number"))
+                inputID = int(input("Please enter your Acc. number\n"))
 
-                if inputID == userID[1] or inputID == userID[2]:
+                if inputID == 1234 or inputID == 2345: # Need to add a better auth method
                     break
                 else:
                     print("Acc No. invalid")
             
             while True:
 
-                menu = int(input(f"Main Menu\n---------\n1. Check Balance\n2. Withdraw\n3. Deposit\n4. Exit\n"))
+                try:
+                    menu = int(input(f"Main Menu\n---------\n1. Check Balance\n2. Withdraw\n3. Deposit\n4. Exit\n"))
+
+                except ValueError:
+                    print("INVALID INPUT - Please try again")
 
                 if menu == 1:
-                    account.accStatus(id, balance)
+                    self.accStatus(id, balance)
                 elif menu == 2:
-                    account.withdraw(id, balance)
+                    self.withdraw(id, balance)
                 elif menu == 3:
-                    account.deposit(id, balance)
+                    self.deposit(id, balance)
                 elif menu == 4:
                     break
                 else:
                     print("INVALID INPUT - Please try again")
             
-Luca = mainMenu(1234, 22)
-
-
-
-
+Luca = ATM(1234, 22)
+Luca.mainMenu(1234, 22)
